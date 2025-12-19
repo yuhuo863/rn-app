a1b260d1e372d29d720010c79d313694e758209c
yuhuo <2581794560@qq.com>
2025-12-20 03:09:38 +0800

    rollback

--- src/utils/queryKeys.js
+++
@@ -1,6 +0,0 @@
-export const QUERY_KEYS = {
-  passwords: ['passwords'], // 密码列表
-  passwordDetail: (id) => ['password', id], // 详情
-  categories: ['categories'], // 分类
-  trash: ['trash'], // 回收站
-}
